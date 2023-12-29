2aa1999a199a5056bba12911c72d080778bee667
Nadir Hossain <nadirhossain336@gmail.com>
2023-12-29 12:20:25 +0600

    header section modify

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from "next/image";
 import Container from "./Container"
 import HoverButton from "./HoverButton";
 
@@ -7,27 +8,39 @@ const Header = () => {
     return (
         <div>
             <div 
-                style={{
-                    backgroundImage: 'url("https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/promo_ss23_endofseasonsale_en_mh_large_d_1a6f64ae86.jpg")',
-                    backgroundPosition: "center",
-                    backgroundRepeat: "no-repeat",
-                    height: "90vh"
-                }}
-                className="relative"
-            >
+                className="
+                    relative
+                    sm:bg-[url(https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_960,w_960/promo_ss23_endofseasonsale_en_mh_large_t_735689fc9d.jpg)]
+                    bg-[url(https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_600,w_600/promo_ss23_endofseasonsale_en_mh_large_m_09e35a6cc9.jpg)]
+                    lg:bg-[url(https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/promo_ss23_endofseasonsale_en_mh_large_d_1a6f64ae86.jpg)]
+                    bg-no-repeat
+                    h-[90vh] !important
+                    object-cover
+                    aspect-auto
+                "
+            > 
                 <Container>
-                    <div className="absolute top-[35%]">
+                    <div className="
+                            absolute
+                            bottom-[5%]
+                            lg:bottom-0 
+                            lg:top-[35%]
+                        "
+                    >
                         <div className="w-fit">
                             <h1 className="text-[40px] font-semibold text-white mb-4">THE END OF YEAR SALE</h1>
                             <div className="flex flex-col gap-3">
                                 <HoverButton
                                     label="SHOP MEN"
+                                    url="/men"
                                 />
                                 <HoverButton
                                     label="SHOP WOMEN"
+                                    url="/women"
                                 />
                                 <HoverButton
                                     label="SHOP KIDS"
+                                    url="/kids"
                                 />
                             </div>
                         </div>
--- app/components/HoverButton.tsx
+++ app/components/HoverButton.tsx
@@ -1,62 +1,67 @@
 'use client'
 
+import Link from "next/link";
 import { BsArrowRight } from "react-icons/bs";
 
 interface HoverButtonProps {
-    label: string
+    label: string,
+    url:string
 }
 
 const HoverButton: React.FC<HoverButtonProps> = ({
-    label
+    label,
+    url
 }) => {
     return (
-        <div className={`
-            ${label === "SHOP WOMEN" ? "w-[175px]" : "w-[150px]"} 
-            relative
-            group 
-        `}>
-            <button className={`
-                bg-white
-                leading-10
-                flex
-                gap-3 
-                pl-3
-                items-center
+        <Link href={url}>
+            <div className={`
                 ${label === "SHOP WOMEN" ? "w-[175px]" : "w-[150px]"} 
                 relative
-                z-20
-                font-bold
-            `}
-            >
-                {label}
-                <BsArrowRight
-                    size={22}
-                    className="
-                        group-hover:ml-[3px] 
-                        transition-all 
-                        duration-[0.2s] 
-                        ease-linear
-                    "
-                />
-            </button>
-            <div className="
-                absolute 
-                top-1 
-                left-1 
-                w-full 
-                h-full 
-                border 
-                border-white
-                group-hover:top-0 
-                group-hover:left-0 
-                transition-all 
-                duration-[0.2s] 
-                ease-linear 
-                z-10"
-                
-            >
+                group 
+            `}>
+                <button className={`
+                    bg-white
+                    leading-10
+                    flex
+                    gap-3 
+                    pl-3
+                    items-center
+                    ${label === "SHOP WOMEN" ? "w-[175px]" : "w-[150px]"} 
+                    relative
+                    z-20
+                    font-bold
+                `}
+                >
+                    {label}
+                    <BsArrowRight
+                        size={22}
+                        className="
+                            group-hover:ml-[3px] 
+                            transition-all 
+                            duration-[0.2s] 
+                            ease-linear
+                        "
+                    />
+                </button>
+                <div className="
+                    absolute 
+                    top-1 
+                    left-1 
+                    w-full 
+                    h-full 
+                    border 
+                    border-white
+                    group-hover:top-0 
+                    group-hover:left-0 
+                    transition-all 
+                    duration-[0.2s] 
+                    ease-linear 
+                    z-10"
+                    
+                >
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
