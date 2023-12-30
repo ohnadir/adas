8ec50057f4a74b9e4e6ad7a7c6ca82a34c4326a8
Nadir Hossain <nadirhossain336@gmail.com>
2023-12-30 13:21:38 +0600

    cart item modify

--- app/components/navbar/Cart.tsx
+++ app/components/navbar/Cart.tsx
@@ -1,15 +1,21 @@
 'use client'
 import { LiaShoppingBagSolid } from "react-icons/lia";
+import { IoBagOutline } from "react-icons/io5";
 import Link from "next/link";
 
 
 const Cart = () => {
+    const bagItemCount= 1;
     return (
-        <Link href='/cart'>
-            <LiaShoppingBagSolid
+        <Link href='/cart' className="relative flex items-center justify-center">
+            <IoBagOutline
                 className="cursor-pointer" 
-                size={25}
+                size={24}
             />
+            {bagItemCount > 0 && (
+                <span className="absolute font-bold text-[10px] top-[32%]">{bagItemCount}</span>
+                )
+            }
         </Link>
     );
 }
