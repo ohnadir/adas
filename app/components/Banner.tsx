c2458e39c57cc437ecda6e96fcb2ef86efed0937
Nadir Hossain <nadirhossain336@gmail.com>
2024-01-02 12:54:38 +0600

    Create Banner component for dynamic product page and cart page

---
+++ app/components/Banner.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+interface BannerProps{
+    details?: string,
+    src?:  React.ReactElement;
+}
+
+const Banner: React.FC<BannerProps> = ({
+    details,
+    src
+}) => {
+    return (
+        <div className="
+            bg-[#f5f5f5] 
+            flex flex-col md:flex-row 
+            items-center justify-center 
+            h-[80px] 
+            leading-5 
+            relative 
+            text-center 
+            text-[15px]
+            gap-1
+            mb-5
+            "
+        >   
+            {src}
+            <span className="max-w-[70%]">{details}</span>
+        </div>
+    )
+}
+
+export default Banner
