2938800aa2356c5ab4fa562305a96e21f1b10c7b
Nadir Hossain <nadirhossain336@gmail.com>
2024-01-02 15:22:53 +0600

    implement summary component for cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,43 @@
+import Banner from "../components/Banner";
+import Button from "../components/Button";
+import CartSummary from "../components/Cart/CartSummary";
+import Container from "../components/Container";
+
+
+const Cart = () => {
+    return (
+        <>
+            {/* banner */}
+            <Banner
+                details="Free delivery for orders SAR 299 and above"
+                src={
+                    <svg fill="none" width="22" height="22" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg"> 
+                    <path d="M14.25 3.75C13.01 3.75 12 4.76 12 6.5V13.5" id="Vector" stroke="#111111" strokeMiterlimit="10" strokeWidth="1.5"></path> 
+                    <path d="M20.25 9.75H3.75" id="Vector_2" stroke="#111111" strokeMiterlimit="10" strokeWidth="1.5"></path> 
+                    <path d="M14.25 3.75H18.64L20.25 9.75V20.25H3.75V9.75L5.36 3.75H10.5" id="Vector_3" stroke="#111111" strokeMiterlimit="10" strokeWidth="1.5"></path> 
+                </svg>
+                }
+            />
+
+            <Container>
+                <div>
+                    {/* cart added product listing section */}
+                    <div></div>
+
+                    {/* cart summary */}
+                    <div>
+                        <CartSummary/>
+                        <Button
+                            label="Checkout"
+                            small
+                            outline
+                        />
+                    </div>
+                </div>
+            </Container>
+            
+        </>
+    )
+}
+
+export default Cart;
---
+++ app/components/Cart/CartSummary.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+interface CartSummaryProps {
+
+}
+
+
+const CartSummary = () => {
+    return (
+        <div className="mb-8">
+            <h1 className="text-xl font-semibold mb-3">Summary</h1>
+            <div className="flex justify-between">
+                <p>Subtotal (9 Items)</p>
+                <div>
+                    <p>SAR 6,350.00</p>
+                    <p className="text-[14px] text-[#757575]">(Inclusive of VAT)</p>
+                </div>
+            </div>
+            <hr className="my-4" />
+            <div className="flex items-center justify-between">
+                <p>Total</p>
+                <p>SAR 6,350.00</p>
+            </div>
+            <hr className="my-4" />
+
+            <div>
+                <p className="text-[14px] font-semibold text-[#757575]">Delivery Fee will be calculated at checkout</p>
+            </div>
+
+            <div className="bg-[#F5F5F5] mt-3 border border-[#d6d6d3] p-2 rounded-md font-semibold">
+                or 4 interest-free payments of SAR 1587.50. No fees. Shariah-compliant.
+                <span className="underline">Learn more</span>
+            </div>
+        </div>
+    )
+}
+
+export default CartSummary
