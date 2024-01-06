c6f0908bd56ba9c4ed31065d249bf81f1717552e
Nadir Hossain <nadirhossain336@gmail.com>
2024-01-06 16:03:43 +0600

    included active status if user is login

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,37 +4,54 @@ import Modal from "../Modals/Modal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import { FaRegCircleUser } from "react-icons/fa6";
+import Link from "next/link";
 
 /* interface UserMenuProps {
     currentUser?: SafeUser | null
 } */
 const UserMenu = () => {
     const registerModal = useRegisterModal();
+    const user = true;
     
     return ( 
         <div className="relative">
-            <FiUser
-                className="cursor-pointer "
-                size={22}
-                onClick={registerModal.onOpen}
-            />
+            {
+                user
+                ?
+                <Link href="/account">
+                    <FaRegCircleUser size={22} />
+                </Link>
+                :
+                <FiUser
+                    className="cursor-pointer "
+                    size={22}
+                    onClick={registerModal.onOpen}
+                />
+            }
             <div className="
                 absolute
                 -top-2
-                -right-[6px] 
+                -right-[8px] 
                 flex 
                 items-center 
                 justify-center
                 "
             >
-                <div className="
-                    active-animation 
-                    h-3 
-                    w-3 
-                    bg-[#22c55eb3] 
-                    rounded-full
-                    "
-                ></div>
+                {
+                    user
+                    ?
+                    <div className="
+                        active-animation 
+                        h-3 
+                        w-3 
+                        bg-[#22c55eb3] 
+                        rounded-full
+                        "
+                    ></div>
+                    :
+                    null
+                }
             </div>
         </div>
     );
